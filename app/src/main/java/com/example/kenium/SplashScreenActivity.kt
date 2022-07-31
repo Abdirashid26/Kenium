070f10a01d5374c5e50cac47d0cf29cbeb5386ed
faisal <anzifai6387@gmail.com>
2022-07-31 08:34:01 +0300

    added the app logo onto the splash screen , and changed the main activtiy into login activity

--- app/src/main/java/com/example/kenium/SplashScreenActivity.kt
+++ app/src/main/java/com/example/kenium/SplashScreenActivity.kt
@@ -1,11 +1,32 @@
 package com.example.kenium
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.view.Window
+import android.view.WindowManager
 
 class SplashScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+
+
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+
+        Handler().postDelayed(
+            {
+                if(true){
+                    val intent = Intent(this, LoginActivity::class.java)
+                    startActivity(intent)
+                    finish()
+                }
+            },5000
+        )
+
     }
 }

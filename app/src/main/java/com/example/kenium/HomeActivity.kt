eca6f117bd3e8dd438689680d6e87c13996844ea
faisal <anzifai6387@gmail.com>
2022-07-31 12:26:34 +0300

    added the login page firebase and created the bottomnavigation and home page for the app

--- app/src/main/java/com/example/kenium/HomeActivity.kt
+++ app/src/main/java/com/example/kenium/HomeActivity.kt
@@ -2,10 +2,45 @@ package com.example.kenium
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class HomeActivity : AppCompatActivity() {
+
+    lateinit var bottomNav: BottomNavigationView
+    private lateinit var currentFragment : Fragment
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        supportFragmentManager.beginTransaction().replace(R.id.fragmentContainer,HomeFragment()).commit()
+
+        bottomNav = findViewById(R.id.bottomNavigation)
+        bottomNav.setOnNavigationItemSelectedListener(navlistener)
+
+
+
+    }
+
+
+
+    val navlistener = BottomNavigationView.OnNavigationItemSelectedListener {
+        when(it.itemId){
+            R.id.Home -> {
+                currentFragment = HomeFragment()
+            }
+            R.id.Search -> {
+                currentFragment = SearchFragment()
+            }
+            R.id.Profile -> {
+                currentFragment = ProfileFragment()
+            }
+            R.id.Downloads -> {
+                currentFragment = DownloadsFragment()
+            }
+        }
+        supportFragmentManager.beginTransaction().replace(R.id.fragmentContainer,currentFragment).commit()
+        true
     }
 }
--- app/src/main/java/com/example/kenium/LoginActivity.kt
+++ app/src/main/java/com/example/kenium/LoginActivity.kt
@@ -3,13 +3,23 @@ package com.example.kenium
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ProgressBar
+import android.widget.TextView
 import android.widget.Toast
 import com.example.kenium.repo.FirebaseRepo
+import com.google.android.material.button.MaterialButton
+import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.textfield.TextInputLayout
 
 class LoginActivity : AppCompatActivity() {
 
 //    FirebaseRepo Initialization
     private val firebaseRepo : FirebaseRepo  = FirebaseRepo()
+    private lateinit var txtEmail : TextInputLayout
+    private lateinit var txtPassword: TextInputLayout
+    private lateinit var btnLogin : MaterialButton
+    private lateinit var progressCircular : ProgressBar
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,7 +30,42 @@ class LoginActivity : AppCompatActivity() {
         if(firebaseRepo.firebaseAuth.currentUser != null){
             val intent = Intent(this,HomeActivity::class.java)
             startActivity(intent)
+            finish()
         }
+
+        txtEmail = findViewById(R.id.Email)
+        txtPassword = findViewById(R.id.Password)
+        btnLogin = findViewById(R.id.loginBtn)
+        progressCircular = findViewById(R.id.progress_circular)
+
+        btnLogin.setOnClickListener {
+            var email = txtEmail.editText?.text
+            var password = txtPassword.editText?.text
+
+            if(email?.isBlank() == true || password?.isBlank() == true){
+                Snackbar.make(it,"Please Enter Username/Password",Snackbar.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            progressCircular.visibility = View.VISIBLE
+
+            firebaseRepo.firebaseAuth.signInWithEmailAndPassword(email.toString(),
+                password.toString()
+            ).addOnCompleteListener { task ->
+                if(task.isSuccessful){
+                    Snackbar.make(it,"Welcome",Snackbar.LENGTH_SHORT).show()
+                    progressCircular.visibility = View.INVISIBLE
+                }
+                else{
+                    Snackbar.make(it,"Wrong Username/Password",Snackbar.LENGTH_SHORT).show()
+                    progressCircular.visibility = View.INVISIBLE
+                }
+            }
+
+
+
+        }
+
 
 
     }

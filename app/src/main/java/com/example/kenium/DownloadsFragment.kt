6f27db23b757cf294ea171f8c0aea9612495b4a7
faisal <anzifai6387@gmail.com>
2022-08-07 11:16:46 +0300

    added extra styling to the item card in the shop page

--- app/src/main/java/com/example/kenium/DownloadsFragment.kt
+++ app/src/main/java/com/example/kenium/DownloadsFragment.kt
@@ -1,13 +1,16 @@
 package com.example.kenium
 
 import android.app.Application
+import android.opengl.Visibility
 import android.os.Bundle
 import android.provider.MediaStore
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ProgressBar
 import androidx.lifecycle.*
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.kenium.adapters.ArticleAdapter
@@ -28,7 +31,8 @@ class DownloadsFragment : Fragment() {
     lateinit var myPhoones:MutableList<Item>
     lateinit var itemAdapter: ItemAdapter
     lateinit var itemAdapter2: ItemAdapter
-
+    lateinit var progress1 : ProgressBar
+    lateinit var progress2 : ProgressBar
 
 
 
@@ -44,6 +48,12 @@ class DownloadsFragment : Fragment() {
 
         itemAdapter = ItemAdapter(requireContext(),myItemsList)
         itemAdapter2 = ItemAdapter(requireContext(),myPhoones)
+
+        progress1 = v.findViewById(R.id.progress_circular)
+        progress2 = v.findViewById(R.id.progress_circular2)
+
+        progress1.visibility = View.VISIBLE
+        progress2.visibility  = View.VISIBLE
 
         sellItems = v.findViewById(R.id.sellitems)
         sellItems2 = v.findViewById(R.id.sellitems2)
@@ -67,6 +77,7 @@ class DownloadsFragment : Fragment() {
 
     suspend fun getAllItems(){
         mFirestore.collection("items").addSnapshotListener { value, error ->
+            progress1.visibility = View.GONE
             if(value != null && error == null){
                 myItemsList.clear()
                 val items = value.toObjects(Item::class.java)
@@ -75,6 +86,7 @@ class DownloadsFragment : Fragment() {
             }
         }
         mFirestore.collection("phones").addSnapshotListener { value, error ->
+            progress2.visibility = View.GONE
             if(value != null && error == null){
                 myPhoones.clear()
                 val items = value.toObjects(Item::class.java)

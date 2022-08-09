945bfb7151e4bdc01ab7265f9d309798c2f1ba32
faisal <anzifai6387@gmail.com>
2022-08-09 15:55:10 +0300

    added the article activity

---
+++ app/src/main/java/com/example/kenium/ArticleActivity.kt
@@ -0,0 +1,11 @@
+package com.example.kenium
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ArticleActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_article)
+    }
+}
